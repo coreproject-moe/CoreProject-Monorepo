3c41a2afcd4438c7069213fc850f475c94416423
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-04-04 22:04:34 +0600

    add

--- backend/django_core/static_src/stores/comment.ts
+++ backend/django_core/static_src/stores/comment.ts
@@ -1,9 +1,9 @@
-import { writable } from "@macfja/svelte-persistent-store";
-
+import { persist, createIndexedDBStorage } from "@macfja/svelte-persistent-store"
+import { writable } from "svelte/store"
 // `url` regex pattern to clear `commentbox_value`
 const whitelisted_regexs = [/^\/anime\/[a-zA-Z0-9]+\/[a-zA-Z0-9]+/gm];
 
-export const commentbox_value = writable<string>("commentbox", "");
+export const commentbox_value = persist(writable<string>("commentbox"),createIndexedDBStorage(), 'commentbox-value');
 
 export function clear_commentbox_if_needed() {
     const matches_string = whitelisted_regexs.some((regex) => {
